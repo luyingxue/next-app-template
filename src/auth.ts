3168881c761bb03778450e54553db821c74374df
luyingxue <luyingxue@126.com>
2025-05-09 11:48:30 +0800

    添加 Supabase 适配器和相关依赖，更新 package.json 和 package-lock.json 文件以支持认证功能。

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,11 @@
-
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
- 
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+  }),
 })
